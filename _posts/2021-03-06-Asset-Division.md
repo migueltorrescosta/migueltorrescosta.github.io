920310ab384c2666f242f1e79f5ae13e9096dcee
Miguel Torres Costa <mcosta@amyris.com>
2021-03-07 12:59:30 +0000

    Working Items list management

--- _posts/2021-03-06-Asset-Division.md
+++ _posts/2021-03-06-Asset-Division.md
@@ -5,8 +5,14 @@ react_component: asset_division
 category: [Tools]
 ---
 
-This tool aims to simplify the process of asset division
+# Goal
+Given a list of assets to be distributed among people, we aim to find a fair distribution taking into account that different people value items differently.
+# Steps
+1. Add the items and an estimated price. This does not need to be correct, it just helps the algorithm with a starting point
+2. Add the list of people wanting to divide the items
+3. Freeze the above lists, so that we can start the division
+4. Each round you will be shown different groupings of the items, and each person should select their favorite group each round. This will allow us to infer item valuations, and suggest the best item allocation.
 
 <div id="asset_division">
-  Hello World
+  <small>Loading Component</small>
 </div>
--- assets/react_components/asset_division.js
+++ assets/react_components/asset_division.js
@@ -4,64 +4,105 @@ const e = React.createElement;
 
 class Items extends React.Component {
     constructor(props) {
-        super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-        this.state = { //state is by default an object
-            students: [
-                { id: 1, name: 'Asif', age: 21, email: 'wasif@email.com' },
-                { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-                { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-                { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-            ]
+        super(props)
+        this.state = {
+            items: [{ id: -1, description: 'Example Item', price: 10 }],
+            new_item: { id: 0, description: '', price: '' }
         }
     }
 
-    renderTableData() {
-        return this.state.students.map((student, index) => {
-            const { id, name, age, email } = student //destructuring
+    renderHeader() {
+        let headerElement = ['Description', 'Approximate Price']
+        return headerElement.map((key, index) => {
+            return <th key={index}>{key}</th>
+        })
+    }
+
+    renderItemsTableData() {
+        return this.state.items.map((item, index) => {
+            const { id, description, price } = item
             return (
                 <tr key={id}>
-                    <td>{id}</td>
-                    <td>{name}</td>
-                    <td>{age}</td>
-                    <td>{email}</td>
+                    <td>{description}</td>
+                    <td>{price}€</td>
+                    <td className='delete_button' width='40px'>
+                        <button className='button' onClick={() => this.removeData(id)}>Delete</button>
+                    </td>
                 </tr>
             )
         })
     }
 
+    renderNewItemForm() {
+        return <form onSubmit={this.handleSubmit.bind(this)}>
+            <div className="form-group row">
+                <label>
+                    Description
+                        </label>
+                <input type="text" value={this.state.new_item.description} name="description" onChange={this.handleDescriptionChange.bind(this)} />
+                <label>
+                    Price
+                </label>
+                <input type="text" value={this.state.new_item.price} name="price" onChange={this.handlePriceChange.bind(this)} />
+                <input type="submit" value="Add New Item" />
+            </div>
+        </form>
+    }
+
+    handleDescriptionChange(e) {
+        this.setState({
+            new_item: {
+                id: this.state.new_item.id,
+                description: e.target.value,
+                price: this.state.new_item.price
+            }
+        })
+    }
+
+    handlePriceChange(e) {
+        this.setState({
+            new_item: {
+                id: this.state.new_item.id,
+                description: this.state.new_item.description,
+                price: parseInt(e.target.value)
+            }
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(e.target)
+        this.setState({
+            items: this.state.items.concat(this.state.new_item),
+            new_item: {
+                id: this.state.new_item.id + 1,
+                description: '',
+                price: '',
+            }
+        })
+    }
+
+    removeData(id) {
+        this.setState({ items: this.state.items.filter(item => item.id != id) })
+    }
+
     render() {
         return (
             <div>
-                <h1 id='title'>React Dynamic Table</h1>
+                <h1 id='title' >Items</h1>
                 <table id='students'>
+                    <thead>
+                        <tr>{this.renderHeader()}</tr>
+                    </thead>
                     <tbody>
-                        {this.renderTableData()}
+                        {this.renderItemsTableData()}
                     </tbody>
                 </table>
+                {this.renderNewItemForm()}
             </div>
         )
     }
 }
-
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { liked: false };
-    }
-
-    render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
-        );
-    }
-}
-
 
 const domContainer = document.querySelector('#asset_division');
 ReactDOM.render(e(Items), domContainer);
