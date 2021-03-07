f0216228f0a2123a445e311533159f9d33a0096a
Miguel Torres Costa <mcosta@amyris.com>
2021-03-07 22:36:49 +0000

    Asset Division fully working prototype

--- _posts/2021-03-06-Asset-Division.md
+++ _posts/2021-03-06-Asset-Division.md
@@ -1,17 +1,19 @@
 ---
-layout: default
+layout: post
 title: Asset Division
 react_component: asset_division
-category: [Tools]
+category: [Interactive]
 ---
 
 # Goal
 Given a list of assets to be distributed among people, we aim to find a fair distribution taking into account that different people value items differently.
 # Steps
-1. Add the items and an estimated price. This does not need to be correct, it just helps the algorithm with a starting point
-2. Add the list of people wanting to divide the items
-3. Freeze the above lists, so that we can start the division
+1. Type in the list of people wanting to divide the items.
+2. Add the items, and for each item an estimated price. <small>This does not need to be exact nor correct as the algorithm re estimates the prices, it just helps to initialize it with sensible values.</small>
+3. Click on "Start" to freeze the above lists and start estimating.
 4. Each round you will be shown different groupings of the items, and each person should select their favorite group each round. This will allow us to infer item valuations, and suggest the best item allocation.
+
+*Warning: Refreshing the page will delete all progress.*
 
 <div id="asset_division">
   <small>Loading Component</small>
--- assets/react_components/asset_division.js
+++ assets/react_components/asset_division.js
@@ -6,11 +6,22 @@ class AssetDivision extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            items: [{ id: -1, description: 'Example Item', price: 10 }],
+            items: [
+                { id: -3, description: 'Sofa', price: 300 },
+                { id: -2, description: 'PC', price: 700 },
+                { id: -1, description: 'Piano', price: 500 }],
             new_item: { id: 0, description: '', price: '' },
-            people: [{ id: -1, name: 'Example Person' }],
+            people: [
+                { id: -2, name: 'Miguel' },
+                { id: -1, name: 'Ricardo' }],
             new_person: { id: 0, name: '' },
             frozen: false, // If true the user can't edit Items nor People
+            group: [-1],
+            group_price_difference: 0,
+            focus_person: 0,
+            individual_prices: Array(),
+            iterations: 0,
+            current_experiment_result: true, // true if the focus_person selects group1, false otherwise
         }
     }
 
@@ -26,7 +37,7 @@ class AssetDivision extends React.Component {
             const { id, description, price } = item
             const delete_cell = (
                 <td className='delete_button' width='40px'>
-                    <form onSubmit={this.handleItemDelete.bind(this)}>
+                    <form>
                         <button className='button' onClick={() => this.handleItemDelete(id)}>Delete</button>
                     </form>
                 </td>
@@ -34,7 +45,7 @@ class AssetDivision extends React.Component {
             return (
                 <tr key={id}>
                     <td>{description}</td>
-                    <td>{price}€</td>
+                    <td>{Math.round(price)}€</td>
                     {!this.state.frozen && delete_cell}
                 </tr>
             )
@@ -52,7 +63,7 @@ class AssetDivision extends React.Component {
                     <input form="add-item" type="text" value={this.state.new_item.price} name="price" onChange={this.handlePriceChange.bind(this)} />
                 </td>
                 <td>
-                    <input form="add-item" type="submit" value="Add New" />
+                    <input form="add-item" type="submit" value="Add Item" />
                 </td>
             </tr>
         )
@@ -133,7 +144,7 @@ class AssetDivision extends React.Component {
             <tr>
                 <td width="40px">
                     <form id="add-person" onSubmit={this.handlePersonSubmit.bind(this)}></form>
-                    <input form="add-person" type="submit" value="Add New" />
+                    <input form="add-person" type="submit" value="Add Person" />
                 </td>
                 <td>
                     <input form="add-person" align="right" type="text" value={this.state.new_person.name} name="name" onChange={this.handlePersonChange.bind(this)} />
@@ -153,7 +164,6 @@ class AssetDivision extends React.Component {
 
     handlePersonSubmit(e) {
         e.preventDefault();
-        console.log(e.target)
         this.setState({
             people: this.state.people.concat(this.state.new_person),
             new_person: {
@@ -170,7 +180,7 @@ class AssetDivision extends React.Component {
     renderPeople() {
         return (
             <div>
-                <h1 id='people-title' >People</h1>
+                <h1 id='people-title'>People</h1>
                 <table id='people'>
                     <thead>
                         <tr></tr>
@@ -184,24 +194,238 @@ class AssetDivision extends React.Component {
         )
     }
 
-    handleStartAlgorithm(e) {
+    startAlgorithm(e) {
         e.preventDefault();
         this.setState({ frozen: true })
+        const item_prices = this.state.items.map(x => x.price);
+        let individual_prices = Array(this.state.people.length).fill(item_prices);
+        individual_prices = individual_prices.map(x => [...item_prices]); // Makes a deep copy of item values
+        for (var i = 0; i < this.state.people.length; ++i) {
+            for (var j = 0; j < this.state.items.length; ++j) {
+                individual_prices[i][j] = individual_prices[i][j] + Math.random() - .5;
+            }
+        }
+        const new_focus_person = (this.state.focus_person + 1) % this.state.people.length;
+        this.setState(
+            {
+                individual_prices: individual_prices,
+                new_focus_person: new_focus_person,
+            },
+            this.setupExperiment);
+    }
+
+    sortedIndices(arr) {
+        var len = arr.length;
+        var indices = new Array(len);
+        for (var i = 0; i < len; ++i) indices[i] = i;
+        indices.sort(function (a, b) { return arr[a] > arr[b] ? -1 : arr[a] < arr[b] ? 1 : 0; });
+        return indices;
+    }
+
+    setupExperiment() {
+        let price_deltas = Array(this.state.items.length);
+        for (var i = 0; i < this.state.items.length; i++) {
+            price_deltas[i] = this.state.individual_prices[this.state.focus_person][i] - this.state.items[i].price
+        }
+        const sorted_indices = this.sortedIndices(price_deltas);
+        let group = [];
+        let group_price = 0;
+        let non_group_price = 0;
+        for (var i = 0; i < price_deltas.length; i++) {
+            const item_position = sorted_indices[i];
+            const item_price = this.state.individual_prices[this.state.focus_person][item_position];
+            if (group_price < non_group_price) {
+                group.push(this.state.items[item_position].id);
+                group_price += item_price;
+            } else {
+                non_group_price += item_price;
+            }
+        }
+        this.setState({
+            group: group,
+            group_price_difference: Math.round(group_price - non_group_price),
+        });
     }
 
     renderStartAlgorithmButton() {
         return (
-            <form align="center" id="start-algorithm" onSubmit={this.handleStartAlgorithm.bind(this)}>
-                <input type="submit" value="Start Algorithm" />
-            </form>
+            <div align="center">
+                <form id="start-algorithm" onSubmit={this.startAlgorithm.bind(this)}>
+                    <input type="submit" value="Start" />
+                </form>
+            </div>
         )
+    }
+
+    getItemsList(group_items, complement) {
+        const custom_filter = complement ? (x => !group_items.includes(x.id)) : (x => group_items.includes(x.id));
+        const return_items = this.state.items.filter(custom_filter).map(x => x.description);
+        let string = "";
+        const group_1_receiving = complement ^ this.state.group_price_difference > 0;
+        const money = Math.round(Math.abs(this.state.group_price_difference / 2));
+        if (group_1_receiving) {
+            string += "Pay " + money.toString() + "€"
+        } else {
+            string += "Get " + money.toString() + "€"
+        };
+        if (return_items.length) {
+            string = return_items.reduce((prev, curr) => prev + ", " + curr, string)
+        }
+        return string;
+    }
+
+    transposeMatrix(arr) {
+        return arr[0].map((_, colIndex) => arr.map(row => row[colIndex]));
+    }
+
+    getPriceAverage(arr) {
+        const transposed_array = this.transposeMatrix(arr);
+        const price_average = transposed_array.map(col => col.reduce((a, b) => a + b, 0) / this.state.people.length);
+        return price_average
+    }
+
+    handleExperimentRun(result) {
+        result.preventDefault;
+        const new_focus_person = (this.state.focus_person + 1) % this.state.people.length;
+        let individual_prices = this.state.individual_prices;
+        const multiplicative_factor = 1 + (1 / (this.state.iterations + 5));
+        for (var i = 0; i < this.state.items.length; i++) {
+            const item = this.state.items[i];
+            const to_increase = !result ^ this.state.group.includes(item.id);
+            if (to_increase) {
+                individual_prices[this.state.focus_person][i] *= multiplicative_factor;
+            } else {
+                individual_prices[this.state.focus_person][i] /= multiplicative_factor;
+            }
+        }
+        const price_average = this.getPriceAverage(individual_prices);
+        let items = this.state.items;
+        for (var i = 0; i < items.length; i++) {
+            items[i].price = price_average[i]
+        }
+        this.setState(
+            {
+                iterations: this.state.iterations + 1,
+                focus_person: new_focus_person,
+                individual_prices: individual_prices,
+                items: items
+            },
+            this.setupExperiment)
+    }
+
+    renderExperiment() {
+        const focus_person_name = this.state.people[this.state.focus_person].name;
+        return (
+            <div>
+                <h1>Round {this.state.iterations}/12</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Group 1</td>
+                            <td>Group 2</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td width="50%">
+                                {this.getItemsList(this.state.group, false)}
+                            </td>
+                            <td width="50%">
+                                {this.getItemsList(this.state.group, true)}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div align="center">
+                    <div>{focus_person_name}</div>
+                    <div><small>which group do you prefer?</small></div>
+                    <input form="experiment" type="button" value="Group 1" name="name" onClick={() => this.handleExperimentRun(true)}></input>
+                    <input form="experiment" type="button" value="Group 2" name="name" onClick={() => this.handleExperimentRun(false)}></input>
+                </div>
+            </div>
+        )
+    }
+
+    getIndices(arr, person_position) {
+        let indices = [];
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == person_position) { indices.push(i) }
+        };
+        return indices
+    }
+
+    renderSuggestedAllocationData() {
+        const transposed_array = this.transposeMatrix(this.state.individual_prices);
+        const item_favourites = transposed_array.map(arr => arr.indexOf(Math.max(...arr)));
+        const average_value = this.state.items.map(x => x.price).reduce((a, b) => a + b, 0) / this.state.people.length;
+        return this.state.people.map((item, index) => {
+            const { id, name } = item
+
+            const preferred_items = this.getIndices(item_favourites, index);
+
+            let value_delta = -average_value;
+            if (preferred_items) {
+                value_delta += preferred_items.map(i => this.state.items[i].price).reduce((a, b) => a + b, 0)
+            }
+
+            let string = "";
+            if (value_delta > 0) {
+                string += "Get " + Math.round(Math.abs(value_delta).toString()) + "€"
+            } else {
+                string += "Pay " + Math.round(Math.abs(value_delta).toString()) + "€"
+            }
+
+            if (preferred_items.length) {
+                string = preferred_items.map(i => this.state.items[i].description).reduce((prev, curr) => prev + ", " + curr, string)
+            }
+
+            return (
+                <tr key={id}>
+                    <td>{name}</td>
+                    <td>{string}</td>
+                </tr>
+            )
+        })
+    }
+
+    renderSuggestedAllocation() {
+        console.log("================");
+        console.log(this.state.iterations);
+        console.log(this.state.individual_prices);
+        console.log(this.state.group_price_difference);
+        return (
+            <div>
+                <h1>Suggested Split</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th width="30%">People</th>
+                            <th>Items</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderSuggestedAllocationData()}
+                    </tbody>
+                </table>
+
+
+            </div>
+        )
+
+    }
+
+    renderItemValuation() {
+
     }
 
     render() {
         return <div>
-            {this.renderItems()}
             {this.renderPeople()}
+            {this.renderItems()}
             {!this.state.frozen && this.renderStartAlgorithmButton()}
+            {this.state.frozen && this.renderExperiment()}
+            {this.state.frozen && this.renderSuggestedAllocation()}
         </div>
     }
 }
